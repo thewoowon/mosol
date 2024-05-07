0a87a15e5f159b887890019c72132379c36ef7ac
thewoowon <thewoowon@naver.com>
2024-05-08 01:14:41 +0900

    feat: app setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import styled from "@emotion/styled";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <Container>
+      <div>못해솔로</div>
+      <div>
+        <button>이런 사람이 좋아</button>
+        <button>이런 사람은 안돼</button>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
