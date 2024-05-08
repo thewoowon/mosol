d39b58aff99bbd580a45f0aca4b8fd3ac0d5ce20
thewoowon <thewoowon@naver.com>
2024-05-08 20:27:42 +0900

    refactor: hobby component removed

--- app/flow/page.tsx
+++ app/flow/page.tsx
@@ -7,7 +7,7 @@ import {
   Height,
   EyeShape,
   Interest,
-  Hobby,
+  // Hobby,
   MBTI,
 } from "@/components/Step";
 import { FlowContextType, Step } from "@/types";
@@ -15,6 +15,11 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import ProgressBar from "@/components/ProgressBar";
 import styled from "@emotion/styled";
+import { BottomButton } from "@/components/Button";
+import { toast } from "react-toastify";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import customAxios from "@/lib/axios";
+import { useDrawerStore } from "@/stores/global";
 
 const stepSequence = [
   "sexAndAge",
@@ -25,7 +30,6 @@ const stepSequence = [
   "faceShape",
   "fashion",
   "interest",
-  "hobby",
 ];
 
 const FlowPage = () => {
@@ -46,6 +50,8 @@ const FlowPage = () => {
       hobby: [],
     },
   });
+
+  const { toggleDrawer, setNode } = useDrawerStore();
 
   const getComponent = (step: string) => {
     const components: {
@@ -107,13 +113,13 @@ const FlowPage = () => {
           flowContext={flowContext}
         />
       ),
-      hobby: (
-        <Hobby
-          key={step}
-          setFlowContext={setFlowContext}
-          flowContext={flowContext}
-        />
-      ),
+      // hobby: (
+      //   <Hobby
+      //     key={step}
+      //     setFlowContext={setFlowContext}
+      //     flowContext={flowContext}
+      //   />
+      // ),
     };
 
     return components[step] || null;
@@ -130,6 +136,90 @@ const FlowPage = () => {
       return { ...prev, step: prevStep as Step, direction: "prev" };
     });
   };
+
+  const validation = () => {
+    if (flowContext.step === "sexAndAge") {
+      // 성별과 나이를 선택하지 않았을 때
+      if (!flowContext.context.sex) {
+        toast.warn("🦄 성별을 선택해주세요!");
+        return false;
+      }
+
+      if (!flowContext.context.age) {
+        toast.warn("🦄 나이를 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "mbti") {
+      if (!flowContext.context.mbti) {
+        toast.warn("🦄 MBTI을 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "lookLike") {
+      if (!flowContext.context.lookLike) {
+        toast.warn("🦄 닮은꼴을 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "height") {
+      if (!flowContext.context.height) {
+        toast.warn("🦄 키를 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "eyeShape") {
+      if (!flowContext.context.eyeShape) {
+        toast.warn("🦄 눈매를 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "faceShape") {
+      if (!flowContext.context.faceShape) {
+        toast.warn("🦄 얼굴형을 선택해주세요!");
+        return false;
+      }
+    } else if (flowContext.step === "fashion") {
+      if (!flowContext.context.fashion) {
+        toast.warn("🦄 패션을 선택해주세요!");
+        return false;
+      }
+    } else {
+      // 활동을 선택하지 않았을 때
+      if (flowContext.context.interest.length === 0) {
+        toast.warn("🦄 활동을 선택해주세요!");
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  const createIdolMutation = useMutation({
+    mutationFn: () => {
+      return customAxios({
+        method: "POST",
+        url: "/result/saveResult",
+        data: {
+          age: flowContext.context.age,
+          sex: flowContext.context.sex,
+          mbti: flowContext.context.mbti,
+          lookLike: flowContext.context.lookLike,
+          height: flowContext.context.height,
+          eyeShape: flowContext.context.eyeShape,
+          faceShape: flowContext.context.faceShape,
+          fashion: flowContext.context.fashion,
+          hobbyList: flowContext.context.interest,
+        },
+      }).then((res: any) => res.data);
+    },
+    onSuccess: (data: any) => {
+      console.log(data);
+      if (data.code === "200") {
+        toast.success("🎉 이상형 생성에 성공했어요!");
+      } else {
+        toast.error("😭 이상형 생성에 실패했어요... 다시 시도해 주세요");
+      }
+    },
+    onError: () => {
+      toast.error("😭 이상형 생성에 실패했어요... 다시 시도해 주세요");
+    },
+  });
 
   return (
     <Container>
@@ -177,6 +267,35 @@ const FlowPage = () => {
           {getComponent(flowContext.step)}
         </motion.div>
       </AnimatePresence>
+      <WidthBlock>
+        <BottomButton
+          onClick={() => {
+            if (!validation()) return;
+            // 마지막 단계일 때
+            if (flowContext.step === "interest") {
+              createIdolMutation.mutate();
+              return;
+            }
+
+            const currentIndex = stepSequence.indexOf(flowContext.step);
+
+            const nextStep = stepSequence[currentIndex + 1] as string;
+            setFlowContext((prev) => {
+              return { ...prev, step: nextStep as Step, direction: "next" };
+            });
+          }}
+          label={flowContext.step === "interest" ? "결과 보기" : "다음"}
+        />
+        {flowContext.step === "lookLike" && (
+          <BottomButton
+            onClick={() => {
+              setNode(<div></div>);
+              toggleDrawer();
+            }}
+            label={"이중에 없어요!"}
+          />
+        )}
+      </WidthBlock>
     </Container>
   );
 };
@@ -209,4 +328,15 @@ const BackArrow = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const WidthBlock = styled.div<{
+  gap?: number;
+}>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: ${(props) => props.gap || 16}px;
 `;
