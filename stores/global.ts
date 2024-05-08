05d6be7ad7ecc6bbadaed783315007f3d51174c9
thewoowon <thewoowon@naver.com>
2024-05-08 20:28:01 +0900

    feat: add zustand store

--- stores/global.ts
+++ stores/global.ts
@@ -5,3 +5,16 @@ export const useStepStore = create((set) => ({
   stepDirection: "next",
   setStep: (step: number) => set({ step }),
 }));
+
+export const useDrawerStore = create<{
+  isOpen: boolean;
+  toggleDrawer: () => void;
+  node: React.ReactNode | null;
+  setNode: (node: React.ReactNode) => void;
+}>((set) => ({
+  isOpen: false,
+  toggleDrawer: () =>
+    set((state: { isOpen: boolean }) => ({ isOpen: !state.isOpen })),
+  node: null,
+  setNode: (node: React.ReactNode) => set({ node }),
+}));
