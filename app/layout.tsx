2cb868f97fd74637a5b7868a5ebb368f506a7659
thewoowon <thewoowon@naver.com>
2024-05-12 12:57:59 +0900

    feat: 네이버 서치 콘솔 주석

--- app/layout.tsx
+++ app/layout.tsx
@@ -165,10 +165,10 @@ export default function RootLayout({
           name="google-site-verification"
           content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
         />
-        <meta
+        {/* <meta
           name="naver-site-verification"
           content={process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION}
-        />
+        /> */}
       </head>
       <body>
         <Analytics />
